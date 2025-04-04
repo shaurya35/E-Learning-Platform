2956c0e95a1cb3ef15cc29ac9c2a558a1c6618c1
OmShankarDeshmukh01 <omdeshmukhstudy@gmail.com>
2025-04-04 15:52:19 +0530

    some edits

--- server/models/StudentModel.js
+++ server/models/StudentModel.js
@@ -21,13 +21,40 @@ const studentSchema = new mongoose.Schema({
   },
   student_department: {
     type: String,
-    required: true
+    required: true,
+    enum:['Computer Engineering',
+          'Electrical Engineering',
+          'Mechanical Engineering',
+          'Civil Engineering',
+          'Chemical Engineering',
+          'Aerospace Engineering',
+          'Biomedical Engineering',
+          'Industrial Engineering',
+          'Computer Science',
+          'Information Technology',
+          'Software Engineering',
+          'Data Science',
+          'Artificial Intelligence',
+          'Cybersecurity',],
+  default :'Computer Engineering'
   },
-  student_enrolledCourses: [{
+  student_degree: {
+    type: String,
+    required: true,
+    enum: ['Bachelor', 'Master', 'PhD', 'Diploma', 'Other'],
+    default: 'Bachelor'
+  },
+  student_semester: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 8
+  },
+  student_enrolled_courses: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Course'
   }],
-},{timestamps:true});
+} , { timestamps: true });
 
 studentSchema.pre('save', function(next) {
   this.updated_at = Date.now();
