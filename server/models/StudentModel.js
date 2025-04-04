3a1a157eb9038bddba92c22112cecc573bdfc595
OmShankarDeshmukh01 <omdeshmukhstudy@gmail.com>
2025-04-04 16:16:59 +0530

    changes in schema

--- server/models/StudentModel.js
+++ server/models/StudentModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const studentSchema = new mongoose.Schema({
   student_id: {
     type: String,
-    required: true,
+    // required: true,
     unique: true
   },
   student_name: {
